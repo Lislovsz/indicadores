85f676838bea619474fceb201534e4ba0a227945
Lislovsz <soaresluud@gmail.com>
2025-08-07 17:34:53 -0300

    Commit inicial

---
+++ script.js
@@ -0,0 +1,15 @@
+const img1 = document.getElementById('image1');
+const img2 = document.getElementById('image2');
+
+let showingFirst = true;
+
+setInterval(() => {
+  if (showingFirst) {
+    img1.style.display = 'none';
+    img2.style.display = 'block';
+  } else {
+    img1.style.display = 'block';
+    img2.style.display = 'none';
+  }
+  showingFirst = !showingFirst;
+}, 10000); // 10000ms = 10 segundos
