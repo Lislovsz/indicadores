7ee2febef86600083abe023eb0f61489b2b4e20e
Lislovsz <soaresluud@gmail.com>
2025-08-07 19:08:14 -0300

    ajuste

--- script.js
+++ script.js
@@ -10,13 +10,12 @@ const images = [
 let currentIndex = 0;
 
 setInterval(() => {
-
+ 
   images.forEach(img => img.style.display = 'none');
 
- 
+
   images[currentIndex].style.display = 'block';
 
- 
+  
   currentIndex = (currentIndex + 1) % images.length;
-}, 30000); 
-
+}, 5000);
