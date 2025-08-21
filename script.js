5f28ba13417d1757608918df751bd54936f613da
Lislovsz <soaresluud@gmail.com>
2025-08-21 14:56:16 -0300

    forçando

--- script.js
+++ script.js
@@ -3,11 +3,6 @@ const images = [
   document.getElementById('image2'),
   document.getElementById('image3'),
   document.getElementById('image4'),
-  document.getElementById('image5'),
-  document.getElementById('image6'),
-  document.getElementById('image7'),
-  document.getElementById('image8'),
-  document.getElementById('image9'),
   document.getElementById('image10')
 ];
 
